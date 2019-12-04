7a4f2c69efff582325e8764cc3695f6f5f00c14a
Rafael Almeida <ralms@ralms.net>
2019-12-04 00:21:53 -0300

    Adjustments

---
+++ AspNet31JsonNamePolicy/Controllers/PeopleController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AspNet31JsonNamePolicy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PeopleController : ControllerBase
+    {
+        private static readonly string[] Peoples = new[]
+        {
+            "Rafael", "Thysbe", "Heloysa", "Eduarda"
+        };
+
+        private readonly ILogger<PeopleController> _logger;
+
+        public PeopleController(ILogger<PeopleController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Person
+            {
+                BirtdayDate = DateTime.Now.AddDays(index),
+                Email = "ralms@ralms.net",
+                FullName = Peoples[rng.Next(Peoples.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ AspNet31JsonNamePolicy/Person.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace AspNet31JsonNamePolicy
+{
+    public class Person
+    {
+        public DateTime BirtdayDate { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
