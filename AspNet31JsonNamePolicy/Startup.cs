4a0ea5654d635fdaa9f6c0b7a6a23dd778c54910
Rafael Almeida <ralms@ralms.net>
2019-12-04 00:02:16 -0300

    Adicionar arquivos de projeto.

---
+++ AspNet31JsonNamePolicy/Startup.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AspNet31JsonNamePolicy
+{
+public class CustomPropertyNamingPolicy : JsonNamingPolicy
+{
+    public override string ConvertName(string name) => ToSnakeCase(name);
+
+    private static string ToSnakeCase(string name)
+    {
+        return string.IsNullOrWhiteSpace(name)
+            ? name
+            : Regex.Replace(
+                name,
+                @"([a-z0-9])([A-Z])",
+                "$1_$2",
+                RegexOptions.Compiled,
+                TimeSpan.FromSeconds(0.2)).ToLower();
+    }
+}
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+public void ConfigureServices(IServiceCollection services)
+{
+    services.AddControllers().AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        options.JsonSerializerOptions.IgnoreNullValues = true;
+        options.JsonSerializerOptions.WriteIndented = false;
+        options.JsonSerializerOptions.AllowTrailingCommas = false;
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+}
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            }); 
+        }
+    }
+}
